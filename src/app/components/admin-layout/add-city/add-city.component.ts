1c3606de9f5491d62a8f7c5e72a91a0203dbfadb
AngleA20 <aya.m.nasar@gmail.com>
2024-07-05 08:02:22 +0300

    Edit Rgion anD Government services and Interfaces

--- src/app/components/admin-layout/add-city/add-city.component.ts
+++ src/app/components/admin-layout/add-city/add-city.component.ts
@@ -1,12 +1,133 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { GovernmentService } from '@service/government.service';
+import { RegionService } from '@service/region.service';
+import { IGovernmentGetDTO } from 'src/app/Interface/IGovernmentGetDTO';
+import { IRegionDTO } from 'src/app/Interface/IRegionDTO';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-add-city',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-city.component.html',
-  styleUrl: './add-city.component.css'
+  styleUrl: './add-city.component.css',
 })
 export class AddCityComponent {
+  cityForm!: FormGroup;
+  governments: IGovernmentGetDTO[] = [];
+  id: Observable<number | null> | undefined;
 
+  constructor(
+    private fb: FormBuilder,
+    private governmentService: GovernmentService,
+    private regionService: RegionService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.cityForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      normalPrice: [0, [Validators.required, Validators.pattern('^[0-9]*$')]],
+      pickupPrice: [0, [Validators.required, Validators.pattern('^[0-9]*$')]],
+      governmentId: [0, Validators.required],
+    });
+
+    this.id = this.route.paramMap.pipe(
+      map((params) => {
+        const idParam = params.get('id');
+        return idParam !== null ? +idParam : 0;
+      })
+    );
+
+    this.id.subscribe((id) => {
+      if (!(id == 0 || id == null)) {
+        this.getById(id);
+      }
+    });
+
+    this.getGovernments();
+  }
+
+  get f() {
+    return this.cityForm.controls;
+  }
+
+  onSubmit() {
+    if (this.cityForm.valid) {
+      let regionDTO: IRegionDTO = this.cityForm.value;
+
+      this.id?.subscribe((id) => {
+        if (id == 0) {
+          this.addCity(regionDTO);
+        } else if (!(id == 0 || id == null)) {
+          this.editCity(this.id, regionDTO);
+        }
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  addCity(regionDTO: IRegionDTO) {
+    this.regionService.addRegion(regionDTO).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {},
+    });
+  }
+
+  editCity(id: any, regionDTO: IRegionDTO) {
+    this.regionService.editRegion(id, regionDTO).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {},
+    });
+  }
+
+  getById(id: any) {
+    this.regionService.getRegionById(id).subscribe({
+      next: (data) => {
+        this.cityForm.patchValue({
+          name: data.name,
+          normalPrice: data.normalPrice,
+          pickupPrice: data.pickupPrice,
+          governmentId: data.governmentId,
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {},
+    });
+  }
+
+
+  getGovernments() {
+    this.governmentService.getAll().subscribe({
+      next: (response) => {
+        this.governments = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {},
+    });
+  }
 }
